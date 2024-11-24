641fc21dab5dab97d7b0228b3d02832e065ee330
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-11-25 01:31:38 +0500

    Initial commit from Create Next App

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='logo'>Rado World</h1>
+      <nav>
+        
+      </nav>
+    </header>
+  )
+}
+
+export default Header
+
